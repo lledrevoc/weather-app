29c2a456dd4d1116ec6ff374479ce9ed72327247
lledrevoc <l.ledrevoc@gmail.com>
2018-10-08 17:54:45 -0600

    small changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express()
 
-const apiKey = 'a358ef34934ae3df59b7419a205cffa3';
+const apiKey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
