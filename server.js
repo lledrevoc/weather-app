2917d03dff65c34a9374d2710423a3c0219a601a
lledrevoc <l.ledrevoc@gmail.com>
2018-10-10 19:52:38 -0600

    Added dockerization

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-const app = express()
+const app = express();
 
-const apiKey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+const apiKey = 'a358ef34934ae3df59b7419a205cffa3';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
   res.render('index', {weather: null, error: null});
-})
+});
 
 app.post('/', function (req, res) {
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+  console.log('Requested ' + city);
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
   request(url, function (err, response, body) {
     if(err){
       res.render('index', {weather: null, error: 'Error, please try again'});
     } else {
-      let weather = JSON.parse(body)
+      let weather = JSON.parse(body);
       if(weather.main == undefined){
         res.render('index', {weather: null, error: 'Error, please try again'});
       } else {
@@ -32,6 +33,11 @@ app.post('/', function (req, res) {
   });
 })
 
+app.get('/about', function (req, res) {
+  res.render('about', {weather: null, error: null});
+});
+
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Example app listening on port 3000!');
+});
